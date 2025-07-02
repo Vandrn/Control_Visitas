79ee61063347cbcfeb3b3b0e873b6bcc8013ec38
vanessa.duran@empresasadoc.com <vanessa.duran@empresasadoc.com>
2025-07-02 16:24:24 -0600

    borrando_archivos que no sirven para el dashboard

--- BackEnd/resources/views/partials/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="footer">
-    <br><br>
-    <img src="{{ asset('images/adoc1.png') }}" alt="ADOC" id="ad">
-    <img src="{{ asset('images/p2.png') }}" alt="Par2" id="p2">
-    <img src="{{ asset('images/hp.png') }}" alt="HushPuppies" id="hp">
-    <img src="{{ asset('images/cat.png') }}" alt="CAT" id="cat">
-    <img src="{{ asset('images/tnf.png') }}" alt="TheNorthFace" id="tnf">
-</footer>
